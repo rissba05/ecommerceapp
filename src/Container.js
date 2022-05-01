7d868829d095048ea2c0c6a8b1a334c778342153
rissba05 <rissba05@gmail.com>
2022-05-01 11:25:10 -0500

    container component code from textbook
    changed default export to more modern convention of React

---
+++ src/Container.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export const Container = ({ children }) => {
+  return (
+    <div style={containerStyle}>
+      {children}
+    </div>
+  );
+}
+
+const containerStyle = {
+  width: 900,
+  margin: '0 auto',
+  padding: '20px 0px'
+};
