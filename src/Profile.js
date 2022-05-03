6fcea1aeed877beb902020c2edfb7f5367de4f7c
rissba05 <rissba05@gmail.com>
2022-05-03 11:41:38 -0500

    Profile component from textbook
    edited to use latest Authenticator component, copied from basic-authentication app from earlier in the semester

---
+++ src/Profile.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './App.css';
+
+import { Authenticator } from '@aws-amplify/ui-react/legacy';
+
+
+export const Profile = () => {
+  return (
+    <div style={containerStyle}>
+        <Authenticator>
+            {({ signOut, user }) => (
+                <main>
+                    <h1>
+                        Hello {user.username}
+                    </h1>
+                    {console.log(user)}
+                    <h2>
+                        Email: {user.attributes.email}
+                    </h2>
+                    <button 
+                        onClick={signOut}
+                    >
+                        Sign out
+                    </button>
+                </main>
+            )}
+        </Authenticator>
+    </div>
+  );
+};
+
+const containerStyle = {
+  width: 400,
+  margin: '20px auto'
+}
